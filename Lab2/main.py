20ccaa42a60edac2c51b8e7014f95e5410c15ed9
adrianaprofirei <100152248+adrianaprofirei@users.noreply.github.com>
2023-10-24 10:15:30 +0300

    Update main.py

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,4 +1,5 @@
 # Ex 1
+#Write a function to return a list of the first n numbers in the Fibonacci string.
 
 # def fibonacci(n):
 #     if n <= 0:
@@ -20,6 +21,7 @@
 
 
 # Ex 2
+#Write a function that receives a list of numbers and returns a list of the prime numbers found in it.
 
 # import math
 #
@@ -46,17 +48,30 @@
 
 
 # Ex 3
+#Write a function that receives as parameters two lists a and b and returns: (a intersected with b, a reunited with b, a - b, b - a)
 
 # def operations(a, b):
-#     intersection = list(set(a) & set(b))
-#     union = list(set(a) | set(b))
-#     aminusb = list(set(a) - set(b))
-#     bminusa = list(set(b) - set(a))
+#     intersection = []
+#     for elem in a:
+#         if elem in b:
+#             intersection.append(elem)
+#     union = a[:]
+#     for elem in b:
+#         if elem not in union:
+#             union.append(elem)
+#     aminusb = []
+#     for elem in a:
+#         if elem not in b:
+#             aminusb.append(elem)
+#     bminusa = []
+#     for elem in b:
+#         if elem not in a:
+#             bminusa.append(elem)
 #
 #     return intersection, union, aminusb, bminusa
 #
-# a = [1, 2, 3, 4]
-# b = [3, 4, 5, 6]
+# a = [2, 3, 6, 3]
+# b = [7, 4, 2, 8, 6, 2]
 # res = operations(a, b)
 # print("Intersection of A and B is: ", res[0])
 # print("Union of A and B is: ", res[1])
@@ -65,6 +80,9 @@
 
 
 # Ex 4
+#Write a function that receives as a parameters a list of musical notes (strings), a list of moves (integers) and a start position (integer). The function will return the song composed by going though the musical notes beginning with the start position and following the moves given as parameter. Example :
+# compose(["do", "re", "mi", "fa", "sol"], [1, -3, 4, 2], 2)
+# will return ["mi", "fa", "do", "sol", "re"]
 
 # def compose(notes, moves, pos):
 #     song = []
@@ -82,6 +100,7 @@
 
 
 # Ex 5
+#Write a function that receives as parameter a matrix and will return the matrix obtained by replacing all the elements under the main diagonal with 0 (zero).
 
 # def replace_under_main_diagonal(matrix):
 #     for i in range(len(matrix)):
@@ -95,6 +114,7 @@
 
 
 # Ex 6
+#Write a function that receives as a parameter a variable number of lists and a whole number x. Return a list containing the items that appear exactly x times in the incoming lists. Example: For the [1,2,3], [2,3,4],[4,5,6], [4,1, "test"] and x = 2 lists [1,2,3 ] # 1 is in list 1 and 4, 2 is in list 1 and 2, 3 is in lists 1 and 2.
 
 # def search_in_list(x, *lists):
 #     combined_lists = []
@@ -121,6 +141,7 @@
 
 
 # Ex 7
+#Write a function that receives as parameter a list of numbers (integers) and will return a tuple with 2 elements. The first element of the tuple will be the number of palindrome numbers
 
 # def palindrome(x):
 #     int_to_string = str(x)
@@ -143,6 +164,9 @@
 
 
 # Ex 8
+#Write a function that receives a number x, default value equal to 1, a list of strings, and a boolean flag set to True. For each string, generate a list containing the characters that have the ASCII code divisible by x if the flag is set to True, otherwise it should contain characters that have the ASCII code not divisible by x. Example:
+# x = 2, ["test", "hello", "lab002"], flag = False
+# will return (["e", "s"], ["e","o"], ["a"]) . Note: The function must return list of lists.
 
 # def ASCII(list, x=None, flag=None):
 #     result = []
@@ -173,7 +197,14 @@
 # print(ASCII(list, x, flag))
 
 # Ex 9
-#
+#Write a function that receives as paramer a matrix which represents the heights of the spectators in a stadium and will return a list of tuples (line, column) each one representing a seat of a spectator which can't see the game. A spectator can't see the game if there is at least one taller spectator standing in front of him. All the seats are occupied. All the seats are at the same level. Row and column indexing starts from 0, beginning with the closest row from the field. Example:
+# FIELD
+# [[1, 2, 3, 2, 1, 1],
+#  [2, 4, 4, 3, 7, 2],
+#  [5, 5, 2, 5, 6, 4],
+#  [6, 6, 7, 6, 7, 5]]
+# Will return : [(2, 2), (3, 4), (2, 4)]
+
 # def spectator_stadium(matrix):
 #     n = len(matrix)
 #     m = len(matrix[0])
@@ -197,6 +228,7 @@
 
 
 # Ex 10
+#Write a function that receives a variable number of lists and returns a list of tuples as follows: the first tuple contains the first items in the lists, the second element contains the items on the position 2 in the lists, etc. Example: for lists [1,2,3], [5,6,7], ["a", "b", "c"] return: [(1, 5, "a ") ,(2, 6, "b"), (3,7, "c")]. Note: If input lists do not have the same number of items, missing items will be replaced with None to be able to generate max ([len(x) for x in input_lists]) tuples.
 
 # def combine_in_tuple(*args):
 #     max_length = max(len(arg) for arg in args)  # Find the maximum length among all input iterables
@@ -218,6 +250,8 @@
 
 
 # Ex 11
+#Write a function that will order a list of string tuples based on the 3rd character of the 2nd element in the tuple. Example:
+# ('abc', 'bcd'), ('abc', 'zza')] ==> [('abc', 'zza'), ('abc', 'bcd')]
 
 # def order_tuple(list):
 #     return sorted(list, key = lambda x: x[1][2])
@@ -226,6 +260,7 @@
 
 
 # Ex 12
+#Write a function that will receive a list of words as parameter and will return a list of lists of words, grouped by rhyme. Two words rhyme if both of them end with the same 2 letters. Example: group_by_rhyme(['ana', 'banana', 'carte', 'arme', 'parte']) will return [['ana', 'banana'], ['carte', 'parte'], ['arme']]
 
 # def group_by_rhyme(words):
 #     res = {}
