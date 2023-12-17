a0b97ca61e64024d7fa8e41f994f9fcf86dc9339
adrianaprofirei <aprofireiadrianmihai1@gmail.com>
2023-12-17 16:03:58 +0200

    added partition path checker

--- Project/analyze_partition.py
+++ Project/analyze_partition.py
@@ -1,1 +1,24 @@
-print("This is my project.")
+import sys
+import psutil
+
+
+def get_partitions():
+    partitions = psutil.disk_partitions()
+    partition_list = [partition.device for partition in partitions]
+    return partition_list
+
+
+def is_valid_partition(partition_arg, partition_list):
+    return partition_arg in partition_list
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise Exception("Wrong number of parameters")
+    else:
+        path = sys.argv[1]
+        if len(path) == 1:
+            path += ":\\"
+        partition_list = get_partitions()
+        if not is_valid_partition(path, partition_list):
+            raise Exception("Specified partition does not exist.")
