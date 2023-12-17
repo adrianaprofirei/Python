7e336d0e6ed64b5abe11ec213e2beb14704fa0af
adrianaprofirei <aprofireiadrianmihai1@gmail.com>
2023-12-17 16:18:25 +0200

    added list all files method

--- Project/analyze_partition.py
+++ Project/analyze_partition.py
@@ -19,6 +19,12 @@ def list_directories(path):
             print(os.path.join(root, dir))
 
 
+def list_files(path):
+    for root, dirs, files, in os.walk(path):
+        for file in files:
+            print(os.path.join(root, file))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise Exception("Wrong number of parameters")
@@ -29,4 +35,5 @@ if __name__ == "__main__":
         partition_list = get_partitions()
         if not is_valid_partition(path, partition_list):
             raise Exception("Specified partition does not exist.")
-        list_directories(path)
+        # list_directories(path)
+        list_files(path)
