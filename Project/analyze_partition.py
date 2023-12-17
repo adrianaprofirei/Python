f68d5782d79210dc0c0c09ae26a496a1e445dd79
adrianaprofirei <aprofireiadrianmihai1@gmail.com>
2023-12-17 16:12:17 +0200

    added list all directories method

--- Project/analyze_partition.py
+++ Project/analyze_partition.py
@@ -1,5 +1,6 @@
 import sys
 import psutil
+import os
 
 
 def get_partitions():
@@ -12,6 +13,12 @@ def is_valid_partition(partition_arg, partition_list):
     return partition_arg in partition_list
 
 
+def list_directories(path):
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            print(os.path.join(root, dir))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise Exception("Wrong number of parameters")
@@ -22,3 +29,4 @@ if __name__ == "__main__":
         partition_list = get_partitions()
         if not is_valid_partition(path, partition_list):
             raise Exception("Specified partition does not exist.")
+        list_directories(path)
